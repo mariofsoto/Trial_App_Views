1870b066d623e4c948cb053ae8d17650e4a2ec4b
fesotini <mfsz2468@gmail.com>
2022-10-07 11:21:17 -0500

    A new view has been added

--- app/src/main/java/com/example/mytickets/HomeActivity.kt
+++ app/src/main/java/com/example/mytickets/HomeActivity.kt
@@ -1,11 +1,24 @@
 package com.example.mytickets
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytickets.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFavoritesImg.setOnClickListener {
+            val intent = Intent(this, FavoriteListActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
